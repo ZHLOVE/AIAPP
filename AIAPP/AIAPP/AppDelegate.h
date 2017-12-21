10252c7c038e8a578cac22e2bbeb5f7647812a6a
zhengyumin <309298713@qq.com>
2017-12-22 00:26:53 +0800

    使用cocoaPod创建项目

---
+++ AIAPP/AIAPP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AIAPP
+//
+//  Created by zhengyumin on 2017/12/22.
+//  Copyright © 2017年 hikvision. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AIAPP/AIAPP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AIAPP
+//
+//  Created by zhengyumin on 2017/12/22.
+//  Copyright © 2017年 hikvision. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
